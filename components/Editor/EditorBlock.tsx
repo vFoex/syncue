fbe7b0ebff8f4f927bae06908aa0943841ebc7d3
vFoex <valentinfoex@gmail.com>
2026-05-20 13:38:27 +0200

    (fix) Fix EditorBlock

--- components/Editor/EditorBlock.tsx
+++ components/Editor/EditorBlock.tsx
@@ -18,7 +18,7 @@ export default function EditorBlock() {
       tools: {
         header: Header,
         paragraph: {
-          class: Paragraph,
+          class: Paragraph as any,
           inlineToolbar: true,
         },
       },
