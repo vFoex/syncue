9e27aee6c21325ecb83b4849ebfb5b6a08801ae0
vFoex <valentinfoex@gmail.com>
2026-05-20 14:28:18 +0200

    (feat) Add speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,6 +53,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <SpeedInsights/>
         {children}
       </body>
     </html>
